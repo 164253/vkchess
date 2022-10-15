6fe5088640e9b60153e265e786ad1b61854435b3
164253 <66006890+164253@users.noreply.github.com>
2022-10-16 02:13:05 +0800

    flask_without_socketio board.js修正後手不能騎動

--- flask_without_socketio/static/board.js
+++ flask_without_socketio/static/board.js
@@ -63,7 +63,7 @@ let bdcli=(x,y)=>{
                     if(y%bdone<bdone/2)nm|=0x80;
                     if(m[81]&16)nm^=0x80;
                     if(ischange){
-                        nm=80-(nm&0x7f);
+                        nm=(nm&0x80)+80-(nm&0x7f);
                         nm^=0x80;
                     }
                     getm(nm,born);
